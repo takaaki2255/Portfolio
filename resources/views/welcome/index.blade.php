64286e7b42b7b5e30214f19c20ba6c528658b961
takaaki2255 <red.ducati.mac2255@gmail.com>
2023-06-13 13:27:32 +0000

    文字の自動調整

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -3,13 +3,13 @@
 @section('content')
 <div class="top">
      <div class="top-brand">
-          <h2 class="top-title" style="font-size:1000%;">
+          <h2 class="top-title" style="font-size:10vw;">
                {{ config('app.name', 'C00KMEMOS') }}
           </h2>
-          <a class="button" style="font-size:300%;" href="{{ url('/portfolio')}}">
+          <a class="button" style="font-size:5vw;" href="{{ url('/portfolio')}}">
                {{ config('app.twoname', 'RECIPE') }}
           </a>
-          <a class="button" style="font-size:300%;" href="{{ url('/profile')}}">
+          <a class="button" style="font-size:5vw;" href="{{ url('/profile')}}">
                {{ config('app.thirdname', 'PROFILE') }}
           </a>
      </div>
